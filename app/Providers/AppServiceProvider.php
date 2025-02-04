2c32f30449e5740b5d9836073493c8477f2eb9a5
EdgarIbarra23 <edgarfercho2309@gmail.com>
2025-02-04 14:20:59 -0500

    feat: migrations

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $migrationPaths = collect(File::allFiles(database_path('migrations')))
+            ->map(fn ($file) => $file->getPath())
+            ->unique()
+            ->toArray();
+
+        $this->loadMigrationsFrom($migrationPaths);
     }
 }
