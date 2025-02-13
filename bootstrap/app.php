fe742848ba3843c5940b8fa5ce6282d5f857852a
EdgarIbarra23 <edgarfercho2309@gmail.com>
2025-02-13 16:58:42 -0500

    feat: message exception Unauthorized

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,19 +1,27 @@
 <?php
 
+use App\Exceptions\UnauthorizedException as ExceptionsUnauthorized;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\JsonResponse;
+use Spatie\Permission\Middleware\RoleMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'role' => RoleMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (AuthenticationException $e, $request): JsonResponse {
+            throw new ExceptionsUnauthorized('No ha iniciado sesión. Por favor, inicie sesión para continuar.');
+        });
     })->create();
